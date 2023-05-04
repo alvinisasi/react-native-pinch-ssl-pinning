4385d7038e1a3e1455b77d72c7d819c4fe9ec6d4
Alvin Miftah <azraqazravin@gmail.com>
2023-05-04 15:58:31 +0700

    fix: error build android

--- android/src/main/java/com/reactlibrary/RNPinchSslPinningPackage.java
+++ android/src/main/java/com/reactlibrary/RNPinchSslPinningPackage.java
@@ -10,6 +10,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+import com.reactlibrary.RNPinch;
 
 public class RNPinchSslPinningPackage implements ReactPackage {
     public List<Class<? extends JavaScriptModule>> createJSModules() {
